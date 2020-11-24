0ed9d6e80ec8e1bc6bded20c2e5d588708ee9daa
Yaroslav <dombrovskiy91@gmail.com>
2020-11-24 12:47:34 +0200

    first step

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul></ul>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vue.js.org"
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Lern to Google...",
+          link: "https://google.org"
+        },
+      ],
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
