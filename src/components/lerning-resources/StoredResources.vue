dd8e3e56ec7f6bd12a928e72b7aadbb4e73e08cf
Yaroslav <dombrovskiy91@gmail.com>
2020-11-27 10:43:03 +0200

    Del btn

--- src/components/lerning-resources/StoredResources.vue
+++ src/components/lerning-resources/StoredResources.vue
@@ -3,6 +3,7 @@
     <lerning-resources
       v-for="res in resources"
       :key="res.id"
+      :id="res.id"
       :title="res.title"
       :description="res.description"
       :link="res.link"
